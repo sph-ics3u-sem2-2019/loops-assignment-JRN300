f7158073fa0091fd49b7dcd9fd66d81300b9a558
JRN300 <joshua.raymond@ocsbstudent.ca>
2019-04-02 11:15:30 -0400

    work

--- src/caesar/Main.java
+++ src/caesar/Main.java
@@ -1,16 +1,28 @@
 package caesar;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in=new Scanner(System.in);
+		System.out.println("Enter a message");
 		String user=in.nextLine();
 		char shift=(char)(user.charAt(0)-'A');
-		System.out.println("Kyle");
-		string name=in.nextLine();
-		if (letter>'A'||letter<='Z')+ shift;
-		
-	}
+		System.out.println("Enter a message");
+		String name=in.nextLine();
+		for (int x=0;x<name.length();x++) {
+			char letter=name.charAt(x);
+			if (letter>'A'||letter<='Z'){
+				letter+=shift;
 
+			}
+			if (letter>'Z') {
+				letter-=26;
+			}
+			System.out.print(letter);
+		}
+
+	}
 }
